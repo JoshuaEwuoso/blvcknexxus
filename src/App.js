50bd55f3bc3e646c343b1b56e3135662cf03364a
JoshuaEwuoso <joe.ewuoso@gmail.com>
2023-05-19 05:34:50 -0400

    uncomment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import React from 'react';
-// import NexusNova from "./pages/NexusNova";
-// import Thrud4Front from './pages/Thrud4Front';
-// import BlvckmenFashion from './pages/BlvckmenFashion';
+import NexusNova from "./pages/NexusNova";
+import Thrud4Front from './pages/Thrud4Front';
+import BlvckmenFashion from './pages/BlvckmenFashion';
 import Home from './pages/Home';
 
 function App() {
@@ -13,9 +13,9 @@ function App() {
         <div>
           <Routes> 
             <Route exact path="/" element={<Home />} />
-            {/* <Route path="/BlvckmenFashion" element={<BlvckmenFashion />} /> */}
-            {/* <Route path="/NexusNova" element={<NexusNova />} /> */}
-            {/* <Route path="/Thrud4Front" element={<Thrud4Front />} /> */}
+            <Route path="/BlvckmenFashion" element={<BlvckmenFashion />} />
+            <Route path="/NexusNova" element={<NexusNova />} />
+            <Route path="/Thrud4Front" element={<Thrud4Front />} />
           </Routes> 
         </div>
       </div>
