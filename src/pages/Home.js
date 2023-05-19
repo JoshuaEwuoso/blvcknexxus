4dcae1a3069dd69e3fc95e797af0fa8c62a395ad
JoshuaEwuoso <joe.ewuoso@gmail.com>
2023-05-19 05:42:11 -0400

    more edits

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,16 +7,8 @@ const Home = () => {
     <header className="App-header">
               <img src={logo} className="App-logo" alt="logo" />
               <p>
-                Edit <code>src/App.js</code> and save to reload.
+              Welcome to BlvckNexxus
               </p>
-              <a
-                className="App-link"
-                href="https://blvcknexxus.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Welcome to BlvckNexxus
-              </a>
               <Link to="/BlvckmenFashion"> BlvckmenFashion </Link>
               <Link to="/NexusNova"> NexusNova </Link>
               <Link to="/Thrud4Front"> Thrud4Front </Link>
