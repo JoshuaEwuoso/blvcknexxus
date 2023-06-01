63123bd6bbf95cea091d3edca7b9de335bc619e4
JoshuaEwuoso <joe.ewuoso@gmail.com>
2023-06-01 06:08:10 -0400

    adds footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer 
+            className="bottom">Copyright &copy; 2023 | GhostPen
+        </footer>
+    );
+  };
+  
+  export default Footer;
