983b308fe3af02e529b0a5f3374b2cebfe72aa01
JoshuaEwuoso <joe.ewuoso@gmail.com>
2023-06-01 06:47:11 -0400

    updates about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,22 +6,17 @@ const About = () => {
         <div>
             <div>
                 <section id="vip">
-                <h1>About Josh</h1>
-                  <p>
-                    Hi & welcome! My name is Joshua Ewuoso, but you can call me Josh. New to the tech field, I began my journey into Pursuit to grow into a Full-Stack Web Engineer.
-                  </p>
+                <h1>Project Description</h1>      
+          <p>
+              Welcome to the digital playground where fashion, tech, and podcasting intertwine to create a vibrant symphony of creativity and knowledge. Our mock website showcases the finest threads that will make heads turn, the cutting-edge gadgets that will ignite your inner geek, and the captivating podcasts that will tickle your auditory senses. With a seamless blend of style, innovation, and intellectual conversations, this portfolio platform invites you to explore the intersection of fashion, technology, and the art of storytelling. So fasten your seatbelts, embrace the trendsetters' spirit, and embark on a journey where fashion meets function, bytes meet beauty, and ideas meet inspiration. Get ready to experience the pinnacle of ingenuity, all in one virtual haven.
+            </p>
                 </section>
             </div>
         <section className="description">
-          <h2>Project Description</h2>
-            <p>
-              This Project lets you search for YouTube videos using the YouTube
-              API, then choose a video from a list to watch. In this project we
-              use Trello Board to track the ToDo lists for this website like
-              functionality, styling, and etch. Furthermore, while we were working
-              on this project we encountered several roadblocks. For example, we
-              learned that some API's like Youtube have limited fetch quotas of 10,000 units.
-            </p>
+          <h2>About Josh</h2>
+          <p>
+              Introducing Josh, the magnetic force behind our digital playground, where fashion, tech, and podcasting converge into an enchanting tapestry of creativity. With an unwavering passion for innovation and an infectious charm, Josh breathes life into our mock website, infusing it with their unique perspective and captivating storytelling. As a trailblazer in the fashion world, a tech aficionado, and a seasoned podcast host, Josh embodies the very essence of the seamless fusion between style, technology, and the art of conversation. Prepare to be mesmerized as Josh takes you on a captivating journey, where each thread, gadget, and spoken word holds the power to ignite your imagination and leave you yearning for more. Embrace the allure of Josh's vision and immerse yourself in the dazzling world they have curated just for you. Get ready to be inspired, informed, and thoroughly entertained as Josh unveils the true magic that lies within the digital realm.
+                  </p>
         </section>
       </div>
     );
