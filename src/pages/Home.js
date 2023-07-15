bc408893ce59971e6ea731db14d02169b5e09e47
joshuaewuoso <joe.ewuoso@gmail.com>
2023-07-14 22:36:35 -0400

    refactor code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,17 +6,16 @@ import logo from "../logo.svg"
 const Home = () => {
   return (
     <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to BlvckNexxus
-        </p>
-        <p>this is the hub where fashion, tech and news comes together</p>
-        <div className="links">
-          <Link onClick={()=> {document.title="BlvckmenFashion"}} to="/BlvckmenFashion"> BlvckmenFashion </Link>
-          <Link onClick={()=> {document.title="NexusNova"}}to="/NexusNova"> NexusNova </Link>
-          <Link onClick={()=> {document.title="Thrud4Front"}}to="/Thrud4Front"> Thrud4Front </Link>
-        </div>
-          
+        <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Welcome to BlvckNexxus
+          </p>
+          <p>This is the hub where fashion, tech and news comes together</p>
+      <div className="links">
+        <Link onClick={()=> {document.title="BlvckmenFashion"}} to="/BlvckmenFashion"> BlvckmenFashion </Link>
+        <Link onClick={()=> {document.title="NexusNova"}}to="/NexusNova"> NexusNova </Link>
+        <Link onClick={()=> {document.title="Thrud4Front"}}to="/Thrud4Front"> Thrud4Front </Link>
+      </div>       
     </div>
   )
 };
