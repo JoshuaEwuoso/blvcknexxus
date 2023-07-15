729fae484f7f6d311c90c54607378b761b41d59f
joshuaewuoso <joe.ewuoso@gmail.com>
2023-07-14 21:02:55 -0400

    test commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -16,7 +16,8 @@ const About = () => {
           <h2>About Josh</h2>
           <p>
               Introducing Josh, the magnetic force behind our digital playground, where fashion, tech, and podcasting converge into an enchanting tapestry of creativity. With an unwavering passion for innovation and an infectious charm, Josh breathes life into our mock website, infusing it with their unique perspective and captivating storytelling. As a trailblazer in the fashion world, a tech aficionado, and a seasoned podcast host, Josh embodies the very essence of the seamless fusion between style, technology, and the art of conversation. Prepare to be mesmerized as Josh takes you on a captivating journey, where each thread, gadget, and spoken word holds the power to ignite your imagination and leave you yearning for more. Embrace the allure of Josh's vision and immerse yourself in the dazzling world they have curated just for you. Get ready to be inspired, informed, and thoroughly entertained as Josh unveils the true magic that lies within the digital realm.
-                  </p>
+          </p>
+          
         </section>
       </div>
     );
