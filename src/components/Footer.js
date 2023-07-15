547d6e842103defc4b2639bb21ced5f3d65a8893
joshuaewuoso <joe.ewuoso@gmail.com>
2023-07-14 22:36:51 -0400

    edits footer info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,28 +8,28 @@ const Footer = () => {
         <h3>Contact Me</h3>
         <p>Got anything to ask?</p>
         <div className="footer-info">
-            <span>contact@blvcknexxus.com</span>
-            <span>USA</span>
-            <span>+1 (858) 058-9558</span>
+            <span>Email us: contact@blvcknexxus.com</span>{' '}
+            <span>Greater New York Area, USA</span>{' '}
+            <span>Call us: +1 (858) 058-9558</span>
         </div>
         <hr/>
       </div>
-    Copyright &copy; 2023 | GhostPen
+    Copyright &copy; 2023 | <a href="https://blvcknexxus.com/About"> GhostPen</a> All Rights Reserved.
     </section>
     {/* <section id="footer" className='mt-5 rounded bg-black py-8 px-8'>
       <div className='text-white md:text-center'>
         <h1 className='font-medium text-lg'>Contact Me</h1>
-        <p>Wanna talk anything?</p>
+        <p>Got anything to ask?</p>
 
         <ul className='md:flex justify-center gap-4'>
           <li className='flex items-center gap-2'>
-            <p>damasukmath@gmail.com</p>
+            <p>blvckNexxus.com@gmail.com</p>
           </li>
           <li className='flex items-center gap-2'>
-            <p>Kab. Tegal</p>
+            <p>Greater New York Area</p>
           </li>
           <li className='flex items-center gap-2'>
-            <p>0858-0058-9558</p>
+            <p>+1-858-058-9558</p>
           </li>
         </ul>
 
@@ -39,8 +39,8 @@ const Footer = () => {
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"/>
 
         <p className="text-center text-sm text-gray-300 sm:text-center">
-          © 2022
-          <a href="https://dendi.ninja"> Damasukma Trihanandi</a>. All Rights Reserved.
+          © 2023
+          <a href="https://blvcknexxus.com/About"> Emini</a>. All Rights Reserved.
         </p>
 
       </div>
