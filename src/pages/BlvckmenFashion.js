37fd5f3526ee3a65c7b6efec3ad5cce34109fbb6
joshuaewuoso <joe.ewuoso@gmail.com>
2023-07-14 22:01:48 -0400

    refactors

--- src/pages/BlvckmenFashion.js
+++ src/pages/BlvckmenFashion.js
@@ -4,7 +4,7 @@ const BlvckmenFashion = () => {
   return (
     <div>
       <h1>Welcome to your path to better dressing</h1>
-      <p>You will love how you look</p>
+        <p>You will love how you look</p>
     </div>
   );
 };
