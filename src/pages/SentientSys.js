6da10bbdf89ba774ddc4a4ec899e8b1f6b0c257f
JoshuaEwuoso <86313521+JoshuaEwuoso@users.noreply.github.com>
2024-03-28 19:45:04 -0400

    header edit

--- src/pages/SentientSys.js
+++ src/pages/SentientSys.js
@@ -3,7 +3,8 @@ import React from 'react';
 const SentientSys = () => {
   return (
     <div>
-      <h1>Welcome to the beginning of your coding journey</h1>
+      <h1>You made it! 
+        Welcome to the beginning of your coding journey</h1>
         <p>Being a nerd is celebrated here</p>
     </div>
   );
