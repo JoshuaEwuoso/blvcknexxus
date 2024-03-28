ff538b7a2ded55109abc7fa1e6d3a66d23c1673a
JoshuaEwuoso <86313521+JoshuaEwuoso@users.noreply.github.com>
2024-03-28 19:32:45 -0400

    remove unnecessary code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,36 +16,6 @@ const Footer = () => {
       </div>
     Copyright &copy; 2023 | <a href="https://blvcknexxus.com/About"> GhostPen</a> All Rights Reserved.
     </section>
-    {/* <section id="footer" className='mt-5 rounded bg-black py-8 px-8'>
-      <div className='text-white md:text-center'>
-        <h1 className='font-medium text-lg'>Contact Me</h1>
-        <p>Got anything to ask?</p>
-
-        <ul className='md:flex justify-center gap-4'>
-          <li className='flex items-center gap-2'>
-            <p>blvckNexxus.com@gmail.com</p>
-          </li>
-          <li className='flex items-center gap-2'>
-            <p>Greater New York Area</p>
-          </li>
-          <li className='flex items-center gap-2'>
-            <p>+1-858-058-9558</p>
-          </li>
-        </ul>
-
-        <div className='flex text-xl text-white gap-4 mt-2 md:justify-center'>
-        </div>
-
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"/>
-
-        <p className="text-center text-sm text-gray-300 sm:text-center">
-          © 2023
-          <a href="https://blvcknexxus.com/About"> Emini</a>. All Rights Reserved.
-        </p>
-
-      </div>
-      
-    </section> */}
         </footer>
     );
   };
