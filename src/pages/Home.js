88708379da19174dcd728c13029dea766e4651d3
JoshuaEwuoso <86313521+JoshuaEwuoso@users.noreply.github.com>
2024-04-25 19:16:30 -0400

    updates div tag

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import React from "react";
 
 
-const Home = () => {
+/*const Home = () => {
   return (
     <div className="App-header">
       <p>Welcome to BlvckNexxus!</p>
@@ -23,6 +23,34 @@ const Home = () => {
               <img src="/thrud4front.png" alt="Thrud4Front" />
             </Link>
           </p>
+      </div>
+    </div>
+  );
+};*/
+
+const Home = () => {
+  return (
+    <div>
+      <div className="App-header">
+        <p className="welcome">Welcome to BlvckNexxus!</p>
+        <p className="descript">This is the hub where Fashion, Tech, and News come together</p>
+      </div>
+      <div className="links">
+        <p>
+          <Link onClick={()=> {document.title="BlvckmenFashion"}} to="/BlvckmenFashion">
+            <img src="/blvckmenfashion.png" alt="BlvckmenFashion" />
+          </Link>
+        </p>
+        <p>
+          <Link onClick={()=> {document.title="SentientSys"}} to="/SentientSys">
+            <img src="/sentientsys.png" alt="SentientSys" />
+          </Link>
+        </p>
+        <p>
+          <Link onClick={()=> {document.title="Thrud4Front"}} to="/Thrud4Front">
+            <img src="/thrud4front.png" alt="Thrud4Front" />
+          </Link>
+        </p>
       </div>
     </div>
   );
