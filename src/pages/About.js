540f5efcb1ce437ef0af6f55d588731d682feb97
JoshuaEwuoso <86313521+JoshuaEwuoso@users.noreply.github.com>
2024-04-25 20:11:38 -0400

    update about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,22 +5,21 @@ const About = () => {
     return (
         <div>
             <div>
-                <section id="vip">
-                <h1>Project Description</h1>      
-          <p>
-              Welcome to the digital playground where fashion, tech, and podcasting intertwine to create a vibrant symphony of creativity and knowledge. Our mock website showcases the finest threads that will make heads turn, the cutting-edge gadgets that will ignite your inner geek, and the captivating podcasts that will tickle your auditory senses. With a seamless blend of style, innovation, and intellectual conversations, this portfolio platform invites you to explore the intersection of fashion, technology, and the art of storytelling. So fasten your seatbelts, embrace the trendsetters' spirit, and embark on a journey where fashion meets function, bytes meet beauty, and ideas meet inspiration. Get ready to experience the pinnacle of ingenuity, all in one virtual haven.
-            </p>
+                <section>
+                  <h1 className="description">Project Description</h1>      
+                    <p className="para">
+                        Welcome to the digital playground where fashion, tech, and podcasting intertwine to create a vibrant symphony of creativity and knowledge. Our mock website showcases the finest threads that will make heads turn, the cutting-edge gadgets that will ignite your inner geek, and the captivating podcasts that will tickle your auditory senses. With a seamless blend of style, innovation, and intellectual conversations, this portfolio platform invites you to explore the intersection of fashion, technology, and the art of storytelling. So fasten your seatbelts, embrace the trendsetters' spirit, and embark on a journey where fashion meets function, bytes meet beauty, and ideas meet inspiration. Get ready to experience the pinnacle of ingenuity, all in one virtual haven.
+                    </p>
                 </section>
             </div>
-        <section className="description">
-          <h2>About Josh</h2>
-          <p>
-              Introducing Josh, the magnetic force behind our digital playground, where fashion, tech, and podcasting converge into an enchanting tapestry of creativity. With an unwavering passion for innovation and an infectious charm, Josh breathes life into our mock website, infusing it with their unique perspective and captivating storytelling. As a trailblazer in the fashion world, a tech aficionado, and a seasoned podcast host, Josh embodies the very essence of the seamless fusion between style, technology, and the art of conversation. Prepare to be mesmerized as Josh takes you on a captivating journey, where each thread, gadget, and spoken word holds the power to ignite your imagination and leave you yearning for more. Embrace the allure of Josh's vision and immerse yourself in the dazzling world they have curated just for you. Get ready to be inspired, informed, and thoroughly entertained as Josh unveils the true magic that lies within the digital realm.
-          </p>
-          
-        </section>
-      </div>
-    );
-  };
+                <section>
+                  <h2 className="intro">About Josh</h2>
+                    <p className="para">
+                      Introducing Josh, the magnetic force behind our digital playground, where fashion, tech, and podcasting converge into an enchanting tapestry of creativity. With an unwavering passion for innovation and an infectious charm, Josh breathes life into our mock website, infusing it with their unique perspective and captivating storytelling. As a trailblazer in the fashion world, a tech aficionado, and a seasoned podcast host, Josh embodies the very essence of the seamless fusion between style, technology, and the art of conversation. Prepare to be mesmerized as Josh takes you on a captivating journey, where each thread, gadget, and spoken word holds the power to ignite your imagination and leave you yearning for more. Embrace the allure of Josh's vision and immerse yourself in the dazzling world they have curated just for you. Get ready to be inspired, informed, and thoroughly entertained as Josh unveils the true magic that lies within the digital realm.
+                    </p>
+                </section>
+            </div>
+  );
+};
 
 export default About;
